a7783016af4670370910a94e790c57951e2cb894
Theriley106 <christopherlambert106@gmail.com>
2019-03-02 16:15:38 -0500

    Fixed lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -18,8 +18,8 @@ def on_intent(intent_request, session):
 	if intent_name == 'bookUber':
 		slots = intent_request["intent"]["slots"]
 		address = slots['address'].get("value", "")
-		cityVal = slots['cityVal'].get("cityVal", "")
-		state = slots['state'].get("state", "")
+		cityVal = slots['cityVal'].get("value", "")
+		state = slots['state'].get("value", "")
 		address = "{} {} {}".format(address, cityVal, state)
 		# whatDay intent
 		return "Book an uber to: {}".format(str(address))
